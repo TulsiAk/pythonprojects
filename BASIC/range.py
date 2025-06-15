5789406203cd6029b022252d525daff19e2e87c2
TulsiAk <tulsiaakanksha2614@gmail.com>
2025-06-15 13:56:01 +0530

    add password genertor

---
+++ BASIC/range.py
@@ -0,0 +1,5 @@
+#sum of all even numbers from 1 to 100
+sum = 0
+for i in range(0, 101,2):
+    sum=sum+i
+print("The sum of all even numbers from 1 to 100 is:", sum)
--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,10 +1,17 @@
-print("1st project")
-input_list = []
-for i in range(1, 4):
-    input_list.append(int(input()))
-max=0
-for i in range(len(input_list)-1):
-    if input_list[i] > max:
-        max = input_list[i]
+import random
+letters=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+']
+password=""
+l=int(input("enter no of letters="))
 
-print("The maximum number is:", max)
+for i in range(1,l+1):
+    password+=random.choice(letters)
+n=int(input("enter no of numbers="))
+for i in range(1,n+1):
+    password+=random.choice(numbers)
+s=int(input("enter no of symbols="))
+for i in range(1,s+1):
+    password+=random.choice(symbols)
+
+print("".join(random.sample(password, len(password))))
